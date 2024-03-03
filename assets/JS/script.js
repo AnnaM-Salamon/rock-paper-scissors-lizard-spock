18203334789416bee4135e8332a77c2a1ed4c978
Anna Maria Salamon <annamariapix@hotmail.com>
2024-03-03 22:48:16 +0000

    Declare functions to update player name, random computer choice, player choice

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,29 +1,27 @@
-//variables//
+
+//variabless
 
 let playerName="Player";
 
-// update name  in score area after player entered a name //
+// update name in game area after player entered a name 
 
 function updatePlayerName() {
     playerName = document.getElementById("playerName").value || "Player";
     document.getElementById("playerNameScore").innerText = playerName;
     }
 
-// choice made by player //
+// choice made by player 
 
     function play(playerChoice) {
         updatePlayerName();
         const computerChoice=getComputerChoice();
 
-
- //  random choice made by computer //
+        document.getElementById("playerNameChoice").innerText = `${playerName} chose ${playerChoice}`;
+        document.getElementById("computerChoice").innerText = `Computer chose ${computerChoice}`;
+     }
+ //  create random choice made by computer 
 
 function getComputerChoice() {
     const choices=["rock","paper","scissors", "lizard", "spock"];
     return choices [Math.floor (Math.random() * choices.length)];
-}
-
-
-   document.getElementById("playerNameChoice").innerText = `${playerName} chose ${playerChoice}`;
-   document.getElementById("computerChoice").innerText = `Computer chose ${computerChoice}`;
 }
