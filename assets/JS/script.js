d24218b2f1a7c5c08f8cb41a01f32d85499b8d00
Anna Maria Salamon <annamariapix@hotmail.com>
2024-03-03 23:05:34 +0000

    Declare function to get results and to update scores

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,6 +2,8 @@
 //variabless
 
 let playerName="Player";
+let playerScore=0;
+let computerScore=0;
 
 // update name in game area after player entered a name 
 
@@ -10,18 +12,55 @@ function updatePlayerName() {
     document.getElementById("playerNameScore").innerText = playerName;
     }
 
-// choice made by player 
 
-    function play(playerChoice) {
-        updatePlayerName();
-        const computerChoice=getComputerChoice();
-
-        document.getElementById("playerNameChoice").innerText = `${playerName} chose ${playerChoice}`;
-        document.getElementById("computerChoice").innerText = `Computer chose ${computerChoice}`;
-     }
  //  create random choice made by computer 
 
-function getComputerChoice() {
-    const choices=["rock","paper","scissors", "lizard", "spock"];
-    return choices [Math.floor (Math.random() * choices.length)];
-}
+    function getComputerChoice(){
+        const choices=["rock","paper","scissors", "lizard", "spock"];
+        return choices [Math.floor (Math.random() * choices.length)];
+   }
+   
+   // choice made by player 
+
+   function play(playerChoice) {
+       updatePlayerName();
+       const computerChoice=getComputerChoice();
+   
+       document.getElementById("playerNameChoice").innerText = `${playerName} chose ${playerChoice}`;
+       document.getElementById("computerChoice").innerText = `Computer chose ${computerChoice}`;
+   
+   // update score area based on results
+
+       const result = getResult(playerChoice, computerChoice);
+       document.getElementById("result").innerText = result;
+   
+       if(result ==="You win!") {
+        playerScore++;
+       }else if (result === "Computer wins!") {
+          computerScore++;
+       }
+   
+       document.getElementById("playerNameScore").innerHTML = `${playerChoice}`;
+       document.getElementById("computerScore").innerHTML = `${computerChoice}`;
+   }
+   
+   // results based on player versus computer choice
+
+   function getResult (player, computer) {
+       if (player === computer) {
+           return "It's a tie";
+       } else if (
+           (player === "rock" && (computer === "scissors" || computer === "lizard")) ||
+           (player === "paper" && (computer === "rock" || computer === "spock")) ||
+           (player === "scissors" && (computer === "paper" || computer === "lizard")) ||
+           (player === "lizard" && (computer === "paper" || computer === "spock")) ||
+           (player === "spock" && (computer === "rock" || computer === "scissors")) 
+       ) {
+           return "You win!";
+       } else {
+           return "Computer wins!";
+       }
+       }
+   
+   
+   
